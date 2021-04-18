602d6971cb1a88ecd7ec65274662658091264b78
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-04-18 15:49:43 -0700

    commit 2

--- change-opacity.js
+++ change-opacity.js
@@ -1,17 +1,17 @@
-AFRAME.registerComponent('change-opacity', {
-    init: function () {
-      this.el.addEventListener('model-loaded', this.update.bind(this));
-    },
-    update: function () {
-      var mesh = this.el.getObject3D('mesh');
-      var opacity = 1.0;
-      if (!mesh) { return; }
-      mesh.traverse(function (node) {
-        if (node.isMesh) {
-          node.material.opacity = opacity;
-          node.material.transparent = opacity < 1.0;
-          node.material.needsUpdate = true;
-        }
-      });
-    }
-  });
+// AFRAME.registerComponent('change-opacity', {
+//     init: function () {
+//       this.el.addEventListener('model-loaded', this.update.bind(this));
+//     },
+//     update: function () {
+//       var mesh = this.el.getObject3D('mesh');
+//       var opacity = 1.0;
+//       if (!mesh) { return; }
+//       mesh.traverse(function (node) {
+//         if (node.isMesh) {
+//           node.material.opacity = opacity;
+//           node.material.transparent = opacity < 1.0;
+//           node.material.needsUpdate = true;
+//         }
+//       });
+//     }
+//   });
