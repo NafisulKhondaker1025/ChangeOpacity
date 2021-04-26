67acbda44e27aabc104054f0badb1871a6b29c30
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-04-26 04:20:14 -0700

    test

--- change-opacity.js
+++ change-opacity.js
@@ -1,17 +1,28 @@
 AFRAME.registerComponent('change-opacity', {
-    init: function () {
-      this.el.addEventListener('model-loaded', this.update.bind(this));
-    },
-    update: function () {
-      var mesh = this.el.getObject3D('mesh');
-      var opacity = 1.0;
-      if (!mesh) { return; }
-      mesh.traverse(function (node) {
-        if (node.isMesh) {
-          node.material.opacity = opacity;
-          node.material.transparent = opacity < 1.0;
+  init: function () {
+    this.el.addEventListener('model-loaded', this.update.bind(this));
+  },
+  update: function () {
+    var mesh = this.el.getObject3D('mesh');
+    if (!mesh) { return; }
+    mesh.traverse(function (node) {
+      if (node.isMesh) {
+        if (node.name === 'walls') {
+          node.material.opacity = 0.5;
+          node.material.transparent = true;
           node.material.needsUpdate = true;
         }
-      });
-    }
-  });
+        if (node.name === 'roof') {
+          node.material.opacity = 0.5;
+          node.material.transparent = true;
+          node.material.needsUpdate = true;
+        }
+        if (node.name === 'floor') {
+          node.material.opacity = 0.5;
+          node.material.transparent = true;
+          node.material.needsUpdate = true;
+        }
+      }
+    });
+  }
+});
