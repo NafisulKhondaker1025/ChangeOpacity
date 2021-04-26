332210e213b6859b8232e44ddc4478159855115c
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-04-26 12:50:50 -0700

    test

--- image-target.js
+++ image-target.js
@@ -29,7 +29,7 @@ AFRAME.registerComponent('physics-image-target', {
           property: 'scale',
           dur: 1500,
           from: '0.001 0.001 0.001',
-          to: '0.5 0.5 0.5',
+          to: '10 10 10',
           easing: 'easeInOutQuad',
         })
       }
