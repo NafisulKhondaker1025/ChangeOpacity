5cbc628b26b77ac674916cebc6cdcea1e3498006
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-07-12 23:39:51 +0600

    test

--- UI.js
+++ UI.js
@@ -1,11 +1,15 @@
 AFRAME.registerComponent('change-sky', {
     init() {
       const container = document.getElementById('container')
-
+      nextbtn = document.createElement('a')
+      nextbtn.innerHTML = "x"
+      nextbtn.classNAme = 'btn'
+      nextbtn.setAttribute('href', 'javascript:void(0)')
       const skyList = ['#pines', '#snow', '#beach']
       var currSky = 0
 
       function setPortal() {
+        console.log('called')
           if (currSky === skyList.length - 1) {
               currSky = 0;
           } else {
@@ -14,8 +18,7 @@ AFRAME.registerComponent('change-sky', {
           var sky = document.getElementById('sky')
           sky.setAttribute('src', skyList[currSky])
       }
-      nextButton = document.getElementById('nxt')
-      nextButton.addEventListener('click', function () {
+      nextbtn.addEventListener('onclick', function () {
         setPortal()
       })
       //nextButton.classList.add('carousel')
